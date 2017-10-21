f438f0960b53ed9dcff82f30f961e41b488ae0bf
Jordan <jld73@students.calvin.edu>
2017-10-21 12:46:08 -0400

    Added unit lists, tank class, and more field data

--- ai/GameState.cs
+++ ai/GameState.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Data;
 
 namespace ai
 {
     public class GameState
     {
         private Map map;
+        private List<Worker> workers;
+        private List<Scout> scouts;
+        private List<Tank> tanks;
+        private List<Worker> enemyworkers;
+        private List<Scout> enemyscouts;
+        private List<Tank> enemytanks;
         
+        
+
     }
 
     class Map
@@ -53,17 +63,37 @@ namespace ai
 
     class Unit
     {
+        public enum State {idle, moving}
+        
         // This unit's position
         private int x, y;
+        // TJis unit's health
+        private int health;
+        // This unit's id
+        private int id;
+        
         // Current command to execute
         private Command cmd;
         // Whether this unit is allied
         private bool ally;
+        // Whether this unit is busy`
+        private State state;
+        // Whether attack is ready
+        private bool attackReady;
+        
+        
 
         private void executeCommand()
         {
             cmd.execute();
         }
+    }
+
+    class Resource
+    {
+        public int x, y;
+        private int id;
+
     }
 
     class Worker : Unit
@@ -75,10 +105,16 @@ namespace ai
     {
         
     }
+    
+    class Tank : Unit
+    {
+        
+    }
 
     abstract class Command
     {
         public abstract void execute();
 
     }
+    
 }
